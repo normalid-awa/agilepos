fdfbeda92164389ee7bb2bec96c74779644bf78e
jkh675 <rluo2018@gmail.com>
2026-08-27 03:33:41 +0800

    feat: Add `Item` interface

---
+++ packages/common/src/item.ts
@@ -0,0 +1,121 @@
+/**
+	An`interface represent an item that could be ordered by usser,
+	could mixed with `IHasImages`
+*/
+export interface IItem {
+	uuid: string;
+	name: string;
+	description?: string;
+	price: number;
+	optionSets?: IOptionSet[];
+}
+
+export interface IHasImages {
+	imagesUrl: string[];
+}
+
+export function hasImages(obj: object): obj is IHasImages {
+	return "imagesUrl" in obj;
+}
+
+export interface IOptionSet {
+	uuid: string;
+	/** Shown to admin only  */
+	name: string;
+	description?: string;
+	options: Option[];
+}
+
+type Option =
+	| {
+			type: "multiple-choice";
+			name: string;
+			description?: string;
+			min?: number;
+			max?: number;
+			choices: OptionChoices;
+	  }
+	| {
+			type: "pick";
+			name: string;
+			description?: string;
+			choices: OptionChoices;
+	  }
+	| {
+			type: "quantities";
+			name: string;
+			description?: string;
+			min?: number;
+			max?: number;
+			/* default min */
+			default?: number;
+	  }
+	| {
+			type: "text";
+			name: string;
+			description?: string;
+			max?: number;
+	  };
+
+type OptionChoices =
+	| {
+			type: "item";
+			choices: {
+				item: IItem;
+				price: number;
+			}[];
+	  }
+	| {
+			type: "preference";
+			choices: {
+				name: string;
+				description?: string;
+				price: number;
+			}[];
+	  };
+
+const TestCokeItem: IItem = {
+	uuid: "2",
+	name: "Coke",
+	price: 12,
+};
+
+const TestHoyCokeItem: IItem = {
+	uuid: "3",
+	name: "HotCoke",
+	price: 6,
+};
+
+const TestOptionDrinkSet: IOptionSet = {
+	uuid: "1",
+	name: "Drinks",
+	options: [
+		{
+			name: "Choose drink",
+			type: "pick",
+			choices: {
+				type: "item",
+				choices: [
+					{
+						item: TestCokeItem,
+						price: 6,
+					},
+					{
+						item: TestHoyCokeItem,
+						price: 3,
+					},
+				],
+			},
+		},
+	],
+};
+
+const TestEggFriedRiceItem: IItem = {
+	uuid: "1",
+	name: "Egg Fried Rice",
+	description: "With egg",
+	price: 60,
+	optionSets: [TestOptionDrinkSet],
+};
+
+console.log(TestEggFriedRiceItem);
