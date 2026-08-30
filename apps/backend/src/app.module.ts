e78b5adc9fc4f3f679bad9f3f05827b7a607a561
jkh675 <rluo2018@gmail.com>
2026-08-30 15:37:40 +0800

    feat(apps/backend): Complate `mikro-orm` init

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,11 +4,14 @@ import { AppService } from "./app.service.js";
 import { AuthModule } from "@thallesp/nestjs-better-auth";
 import { auth } from "./auth.js";
 import { ConfigModule } from "@nestjs/config";
+import { MikroOrmModule } from "@mikro-orm/nestjs";
+import config from "../mikro-orm.config.js";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
 		AuthModule.forRoot(auth, { bodyParser: { rawBody: true } }),
+		MikroOrmModule.forRoot(config),
 	],
 	controllers: [AppController],
 	providers: [AppService],
